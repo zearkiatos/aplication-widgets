3b062cad7e3741caaf017b5df464d00896c0991e
Pedro Capriles <zearkiatos@Pedros-MacBook-Air.local>
2020-12-13 23:22:33 -0300

    Helper Functions in Function Components

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(item => {
+    const onTitleClick = (index) => {
+        console.log('Title Clicked', index)
+    };
+    const renderedItems = items.map((item, index) => {
         return (
             <Fragment key={item.title}>
-                <div className="title active">
+                <div className="title active" onClick={() => onTitleClick(index)}>
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>

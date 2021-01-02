ce2306852640818cd8d5ca501b5209c801aa1f37
Pedro Capriles <zearkiatos@Pedros-MacBook-Air.local>
2021-01-02 18:34:04 -0300

    The useEffect Hook

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
     const [term, setTerm] = useState('');
+
+    useEffect(() => {
+        console.log('I am a useEffect hook');
+    }, [term]);
     return (
         <div>
             <div className="ui form">

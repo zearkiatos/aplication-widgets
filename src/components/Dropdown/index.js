4f881cf6bb46cf69170ac7ef34b92bbad1c2abb0
Pedro Capriles <zearkiatos@Pedros-MacBook-Air.local>
2021-01-23 13:57:47 -0300

    Selection State

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, selected, onSelectedChange }) => {
     const renderedOptions = options.map((option) => {
         return (
-            <div key={option.value} className="item">
+            <div 
+                key={option.value} 
+                className="item"
+                onClick={() => onSelectedChange(option)}
+            >
                 {option.label}
             </div>
         );
@@ -14,7 +18,7 @@ const Dropdown = ({ options }) => {
                 <label className="label">Select a Color</label>
                 <div class="ui selection dropdown visible active">
                     <i className="dropdown icon"></i>
-                    <div className="text">Select Color</div>
+                    <div className="text">{selected.label}</div>
                     <div className="menu visible transition">{renderedOptions}</div>
                 </div>
             </div>

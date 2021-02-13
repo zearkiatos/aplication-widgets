d61ed3e859b00e2a7060c5dcef3ef2cfc6f7cc5c
Pedro Capriles <caprilespe@outlook.com>
2021-02-13 17:14:54 -0300

    Displaying translated text

--- src/components/Convert/index.js
+++ src/components/Convert/index.js
@@ -3,16 +3,25 @@ import axios from 'axios';
 import config from '../../constants/config';
 
 const Convert = ({ language, text }) => {
+    const [translated, setTranslated] = useState('');
     useEffect(() => {
-        axios.post(config.GOOGLE_TRANSLATE_API, {}, {
-            params: {
-                q: text,
-                target: language.value,
-                key: config.GOOGLE_TRANSLATE_API_KEY
-            }
-        });
+        const doTranslation = async () => {
+            const { data } = await axios.post(config.GOOGLE_TRANSLATE_API, {}, {
+                params: {
+                    q: text,
+                    target: language.value,
+                    key: config.GOOGLE_TRANSLATE_API_KEY
+                }
+            });
+            setTranslated(data.data.translations[0].translatedText);
+        };
+        doTranslation();
     }, [language, text])
-    return <div />;
+    return (
+        <div>
+            <h1 className="ui header">{translated}</h1>
+        </div>
+    );
 };
 
 export default Convert;
